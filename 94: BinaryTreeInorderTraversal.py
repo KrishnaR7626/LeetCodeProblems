28a536a969d96746815be9df584b2eaf69850fd6
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-07-12 18:05:29 -0400

    Create 94: BinaryTreeInorderTraversal.py

---
+++ 94: BinaryTreeInorderTraversal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        arr = []
+        if root is not None: 
+            arr = self.inorderTraversal(root.left)
+            r = self.inorderTraversal(root.right)
+            arr.append(root.val)
+            arr.extend(r)
+        return arr
