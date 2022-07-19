bffe213cd3bd56f51885f3e02e3c6e5079183152
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-07-19 14:48:33 -0400

    Create python.py
    Python Solution

---
+++ 69: Sqrt(x)/python.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/sqrtx/
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0 or x == 1:
+            return x
+        i = 1
+        while i < x:
+            if ((i+1) * (i+1) > x) or (i * i == x):
+                return i 
+            i+=1
+                
