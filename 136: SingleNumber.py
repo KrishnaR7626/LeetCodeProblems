a83d9bc7bc319acacc423004b711796d53c9f1b7
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-07-21 16:45:46 -0400

    Update 136: SingleNumber.py
    Added annotations to code

--- 136: SingleNumber.py
+++ 136: SingleNumber.py
@@ -1,17 +1,22 @@
+# https://leetcode.com/problems/single-number/
 class Solution(object):
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        #Sort the list to group all the double elements to gether
         nums.sort()
+        #if there is only 1 element return it
         if(len(nums) == 1):
             return nums[0]
-
         i = 1
+        #iterate through the loop
         while i < len(nums):
+            #if the 2 elements next to each other are not the same return the different element.
             if not nums[i] == nums[i-1]:
                 return nums[i-1]
             i+=2
+        #return the last element.
         return nums[len(nums)-1]
         
