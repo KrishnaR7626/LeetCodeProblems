7e4be88b61b4685d9d6d6194fd00ab8b3980ba82
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-07-21 17:35:36 -0400

    Update 226: InvertBinaryTree.java
    Added annotations and small optimization

--- 226: InvertBinaryTree.java
+++ 226: InvertBinaryTree.java
@@ -1,20 +1,20 @@
-https://leetcode.com/problems/invert-binary-tree/submissions/
+// https://leetcode.com/problems/invert-binary-tree/submissions/
 class Solution {
     public TreeNode invertTree(TreeNode root) {
+//         checks if current node is null
         if(root == null){
             return null;
         }
-        if(root.left == null){
-            root.left = null;
-        }
-        if(root.right == null){
-            root.right = null;
-        }
+//          copies the left node into a temporary object 
         TreeNode node = root.left;
+//         puts right node in left
         root.left = root.right;
+//         puts the left node copy in right node
         root.right = node;
+//         recursively calls the inversion on the child nodes.
         this.invertTree(root.left);
         this.invertTree(root.right);
+//         returns the root node
         return root;
     }
 }
