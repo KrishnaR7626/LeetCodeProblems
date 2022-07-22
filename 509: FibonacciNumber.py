0da19efee5965becc81e9ccb81a54a15f78f7bc7
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-07-22 16:51:13 -0400

    Update 509: FibonacciNumber.py
    Added annotations and optimized

--- 509: FibonacciNumber.py
+++ 509: FibonacciNumber.py
@@ -1,9 +1,9 @@
 #https://leetcode.com/problems/fibonacci-number/submissions/
 class Solution(object):
     def fib(self, n):# arg: int, return: int
+#       if n > 2 recursively call and findout the value of last 2 elements.
         if n >= 2:
             return self.fib(n-1) + self.fib(n-2)
-        elif n == 1:
-            return 1
-        elif n == 0:
-            return 0
+#       if n == 1 or 0 return 1 or 0
+        return n
+
