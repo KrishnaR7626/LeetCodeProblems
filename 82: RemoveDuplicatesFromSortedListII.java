57393e835aa389c04db0e031bc8843086dda1671
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-08-19 16:24:29 -0400

    Update 82: RemoveDuplicatesFromSortedListII.java
    Added annotations and cleaned code

--- 82: RemoveDuplicatesFromSortedListII.java
+++ 82: RemoveDuplicatesFromSortedListII.java
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/submissions/
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,30 +11,36 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null || head.next == null ){
-            return head;
-        }
+        // initializes 2 pointers
         ListNode current = head;
         ListNode prev = null;
+        int num;
+        // traverses the linked list
         while(current != null){
+              // if the value of current and next node is same
               if(current.next != null && current.val == current.next.val){
-                  int num = current.val;
+                  // stores value in local variable
+                  num = current.val;
+                  // moves until the value is not the same
                   while(current != null && current.val == num){
                       current = current.next;
                   }
+                  // if there are no nodes left and if there are no other values in the list other than the duplicates, returns null
+                  if(current == null && prev  == null){
+                    return current;
+                  }
+                  // if there are no nodes left terminates the list
+                  if(current == null){
+                    prev.next = null;
+                  }
+                  // if prev pointer is null sets head to current position to remove all previous positions, else sets prev pointer to current position
                   if(prev == null){
                      head = current; 
                   }else{
-                      if(current == null && prev  == null){
-                        head = null;
-                        return head;
-                      }else if(current == null){
-                        prev.next = null;
-                        return head;
-                      }
                       prev.next = current;
-                  }
-              }else{                  
+                  }                                        
+              }else{
+                  // iterates through the array
                   prev = current;
                   current = current.next;
               }
