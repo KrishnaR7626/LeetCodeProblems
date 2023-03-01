5abdbe643c73acb5cab01fc559d22bea71db4b30
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2023-03-01 14:00:22 -0500

    Create c2.c

---
+++ 69: Sqrt(x)/c2.c
@@ -0,0 +1,19 @@
+int mySqrt(int x){
+    int l = 0;
+    int h = x;
+    long mid;
+    long long res;
+    while(l<=h){
+        mid = ((long)l+h)/2;
+        res = mid*mid;
+        if(res < x){
+            l = mid+1;
+        }else if(res > x){
+            h = mid-1;
+        }else{
+            return mid;
+        }
+    }
+    return h;
+    
+}
